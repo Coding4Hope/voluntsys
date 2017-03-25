50ba44ed796490082835bbb8e5d44580cadfd99f
lovieira <luk14236@gmail.com>
2017-03-25 13:29:55 -0300

    enviando os novos métodos de js

--- core/views.py
+++ core/views.py
@@ -2,38 +2,161 @@
 from django.shortcuts import render, redirect,render_to_response
 from django.http.response import HttpResponse
 from django.views.generic.base import View
+from django.core.serializers import serialize
+from django.views.decorators.csrf import csrf_exempt
 from core.models import *
 
-def home(request):
-    context = {}
-    return render(request, 'home.html', context)
+class homeView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'home.html', context)
 
 
-def associados(request):
-    context = {}
-
-    lista_associados = Associado.objects.all()
-
-    context["associados"] = lista_associados
-
-    return render(request, 'associados/associados_geral.html', context)
+class associadosView(View):
+    def get(self,request, *args, **kwargs):
+        return render(request, 'associados/associados_geral.html', {})
 
 
-class VoluntarioNovoView(View):
+class assiciadoNovoView(View):
     def get(self,request, *args, **kwargs):
         context = {}
-
         return render(request, 'associados/associado_novo.html', context)
-    def post(self,request, *args, **kwargs):
-        oIntegrante = Integrante.objects.get(id=request.GET.get("id"))
 
-        oIntegranteHierarquia = IntegranteHierarquia()
-        oIntegranteHierarquia.integrante = oIntegrante
-        oIntegranteHierarquia.data_entrega = request.POST.get("data")
-        oIntegranteHierarquia.hierarquia=Hierarquia.objects.get(id=request.POST.get("hierarquia_selecionado"))
-        oIntegranteHierarquia.save()
 
-        oIntegrante.hierarquia_atual = oIntegranteHierarquia.hierarquia
-        oIntegrante.save()
+class associadoExcluirView(View):
+    def get(self,request, *args, **kwargs):
+        return render(request, 'associados/associado_excluir.html', {})
 
-        return redirect('/integrantes')
+class tiposassociadosView(View):
+    def get(self,request, *args, **kwargs):
+        return render(request, 'tiposassociados/tipoassociados_geral.html', {})
+
+
+class tipoassiciadoNovoView(View):
+    def get(self,request, *args, **kwargs):
+        context = {}
+        return render(request, 'tipoassociados/tipoassociado_novo.html', context)
+
+
+class tipoassociadoExcluirView(View):
+    def get(self,request, *args, **kwargs):
+        return render(request, 'tipoassociados/tipoassociado_excluir.html', {})
+
+
+class ServiceJson(View):
+    @staticmethod
+    def associados(request):
+        query = Associado.objects.all()
+
+        id = request.GET.get("id")
+        nome = request.GET.get("nome")
+        email = request.GET.get("email")
+        tipo_associado = request.GET.get("tipo_associado")
+
+        if id:
+            query = query.filter(id=id)
+        if nome:
+            query = query.filter(nome__icontains=nome)
+        if email:
+            query = query.filter(email__icontains=email)
+        if tipo_associado:
+            query = query.filter(tipo_associacao__id=tipo_associado)
+
+        lista = serialize('json', query)
+        return HttpResponse(lista, content_type='application/json')
+
+    @staticmethod
+    @csrf_exempt
+    def saveassociado(request):
+        # Filtros
+        id = request.POST.get("id")
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        endereco = request.POST.get("endereco")
+        telefone = request.POST.get("telefone")
+        tipo_associado = request.POST.get("tipo_associado")
+        bairro = request.POST.get("bairro")
+
+
+        # Objeto de Associado
+        Associado = Associado()
+
+        if (id):
+            if (int(id) > 0):
+                oAssociado = Associado.objects.get(id=id)
+
+        oAssociado.nome = nome
+        oAssociado.email = email
+        oAssociado.endereco = endereco
+        oAssociado.telefone = telefone
+        oAssociado.tipo_associado = TipoAssociado.objects.filter(id=tipo_associado).first()
+        oAssociado.bairro = Bairro.objects.filter(id=bairro).first()
+
+        oAssociado.save()
+
+        lista = "true"
+        return HttpResponse(lista, content_type='application/json')
+
+
+    @staticmethod
+    def excluirassociado(request):
+        id = request.GET.get("id")
+
+        # Query Base
+        oAssociado = Associado.objects.filter(id=id).first()
+
+        oAssociado.delete()
+
+        lista = "true"
+
+        return HttpResponse(lista, content_type='application/json')
+
+
+    @staticmethod
+    def tiposassociados(request):
+        query = TipoAssociado.objects.all()
+
+        id = request.GET.get("id")
+        titulo = request.GET.get("titulo")
+
+        if id:
+            query = query.filter(id=id)
+        if titulo:
+            query = query.filter(titulo__icontains=titulo)
+
+        lista = serialize('json', query)
+        return HttpResponse(lista, content_type='application/json')
+
+    @staticmethod
+    @csrf_exempt
+    def savetipoassociado(request):
+        # Filtros
+        id = request.POST.get("id")
+        titulo = request.POST.get("nome")
+
+
+        # Objeto de Associado
+        oTipoAssociado = TipoAssociado()
+
+        if (id):
+            if (int(id) > 0):
+                oTipoAssociado = TipoAssociado.objects.get(id=id)
+
+        oTipoAssociado.titulo = titulo
+        oTipoAssociado.save()
+
+        lista = "true"
+        return HttpResponse(lista, content_type='application/json')
+
+
+    @staticmethod
+    def excluirtipoassociado(request):
+        id = request.GET.get("id")
+
+        # Query Base
+        oTipoAssociado = TipoAssociado.objects.filter(id=id).first()
+        oTipoAssociado.delete()
+
+        lista = "true"
+
+        return HttpResponse(lista, content_type='application/json')
--- voluntsys/urls.py
+++ voluntsys/urls.py
@@ -21,7 +21,23 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^associados/$', associados),
-    url(r'^associado-novo/$', VoluntarioNovoView.as_view(), name ='VoluntarioNovo'),
+    url(r'^$', homeView.as_view()),
+    url(r'^associados/$', associadosView.as_view(), name="Voluntarios-adm"),
+    url(r'^associado-novo/$', assiciadoNovoView.as_view(), name ='VoluntarioNovo-adm'),
+    url(r'^associado-excluir/$', associadoExcluirView.as_view(), name ='VoluntarioExcluir-adm'),
+
+    url(r'^tiposassociados/$', tiposassociadosView.as_view(), name="Voluntarios-adm"),
+    url(r'^tipoassociado-novo/$', tipoassiciadoNovoView.as_view(), name='VoluntarioNovo-adm'),
+    url(r'^tipoassociado-excluir/$', tipoassociadoExcluirView.as_view(), name='VoluntarioExcluir-adm'),
+
+    url(r'^js/associados/$', ServiceJson.associados, name="associados"),
+    url(r'^js/tiposassociados/$', ServiceJson.tiposassociados, name="tiposassociados"),
+
+    url(r'^js/saveassociado', ServiceJson.saveassociado, name='saveassociado'),
+    url(r'^js/savetipoassociado', ServiceJson.savetipoassociado, name='savetipoassociado'),
+
+    url(r'^js/excluirassociado', ServiceJson.excluirassociado, name='excluirassociado'),
+    url(r'^js/excluirtipoassociado', ServiceJson.excluirtipoassociado, name='excluirtipoassociado'),
+
+
 ] + static(settings.STATIC_URL, document_root=settings.BASE_DIR)
